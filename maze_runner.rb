957167606f2522fa7fbc6a9f337f5eb0d80edf1b
JC Wilcox <84jwilcox@gmail.com>
2014-12-21 00:02:56 -0600

    Add OptionParser
    Change maze_runner to accept named command line options for height,
    width, display time, and iterations. Also added a '--help' flag to view
    available options.

--- maze_runner.rb
+++ maze_runner.rb
@@ -1,17 +1,72 @@
+require 'optparse'
 require './maze'
 
-if __FILE__ == $0
-  height = (ARGV[0] || 25).to_i
-  width = (ARGV[1] || 40).to_i
-  display_time = (ARGV[2] || 0.005).to_f
-  loops = ARGV[3] ? ARGV[3].to_i : Float::INFINITY
-
-  puts
-  (0...loops).each do
-    m = Maze.new(height, width, display_time).make.solve
-    sleep(2)
-    m.erase
+module MazeRunner
+  class << self
+    attr_accessor :options
   end
 
-  puts "Hope you enjoyed the show!"
+  @options = {
+    height: 25,
+    width: 40,
+    display_time: 0.005,
+    iterations: Float::INFINITY
+  }
+
+  def self.parse(args)
+    option_parser.parse!(args)
+  end
+
+  def self.maze
+    Maze.new(options[:height], options[:width], options[:display_time])
+  end
+
+  def self.run
+    puts
+    (0...options[:iterations]).each do
+      m = maze.make.solve
+      sleep(2)
+      m.erase
+    end
+
+    puts "Hope you enjoyed the show!"
+  end
+
+  private
+
+  def self.option_parser
+    OptionParser.new do |opts|
+      opts.banner = "Usage: ruby maze_runner.rb [options]"
+
+      opts.separator ''
+      opts.separator 'Specific options:'
+
+      opts.on('-h', '--height HEIGHT', Integer, 'Height of the maze') do |h|
+        options[:height] = h
+      end
+
+      opts.on('-w', '--width WIDTH', Integer, 'Width of the maze') do |w|
+        options[:width] = w
+      end
+
+      opts.on('-d', '--display-time N', Float,
+        'Display each step N seconds') do |n|
+          options[:display_time] = n
+        end
+
+      opts.on('-i', '--iterations N', Integer, 'Display N mazes') do |n|
+        options[:iterations] = n
+      end
+
+      opts.on_tail('--help', "Show this message") do
+        puts opts
+        exit
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  MazeRunner.parse(ARGV)
+  MazeRunner.run
 end
