181a0002f8d0ef00bd95393256063e88fb61c890
JC Wilcox <84jwilcox@gmail.com>
2015-01-20 09:38:35 -0600

    Refactor BreadthSolver
    Break the logic of `#build_path` into a series of private methods.

--- breadth_solver.rb
+++ breadth_solver.rb
@@ -1,5 +1,5 @@
 class BreadthSolver < Solver
-  attr_reader :current_paths
+  attr_reader :current_paths, :sub_paths
 
   def initialize(*args)
     @current_paths = []
@@ -14,25 +14,14 @@ class BreadthSolver < Solver
   end
 
   def build_path
-    current_paths.each { |p| solution << p.last.visit.dup unless p.last.visited? }
+    visit_current_nodes
+
     if finale
       mark_final_dead_ends
       return true
     end
 
-    sub_paths = []
-    current_paths.each_with_index do |current_path, i|
-      if branches(current_path).empty?
-        current_paths.values_at(0...i, (i + 1)..-1).each do |p|
-          current_path.nodes -= p.nodes
-        end
-        current_path.nodes.reverse.each do |n|
-          solution << n.dead_end unless n.dead_end?
-        end
-      else
-        sub_paths.concat(branches(current_path))
-      end
-    end
+    evaluate_branches
 
     unless sub_paths.empty?
       @current_paths = sub_paths
@@ -40,19 +29,42 @@ class BreadthSolver < Solver
     end
   end
 
+  def visit_current_nodes
+    current_paths.each { |p| solution << p.last.visit.dup unless p.last.visited? }
+  end
+
   def finale
     current_paths.find { |p| p.last == maze.finish }
   end
 
   def mark_final_dead_ends
-    (current_paths.flat_map(&:nodes) - finale.nodes).reverse.each do |n|
-      solution << n.dead_end unless n.dead_end?
+    mark_dead_end(current_paths.flat_map(&:nodes) - finale.nodes)
+  end
+
+  def mark_dead_end(nodes)
+    nodes.reverse_each { |n| solution << n.dead_end unless n.dead_end? }
+  end
+
+  def evaluate_branches
+    @sub_paths = []
+    current_paths.each do |current_path|
+      if branches(current_path).empty?
+        mark_dead_end(unique_nodes(current_path))
+      else
+        sub_paths.concat(branches(current_path))
+      end
     end
   end
 
   def branches(path)
-    unvisited_neighbors(path.last).map do |n|
-      Path.new(path.nodes, n)
-    end
+    unvisited_neighbors(path.last).map { |n| Path.new(path.nodes, n) }
+  end
+
+  def unique_nodes(path)
+    path.nodes - other_paths(path).flat_map(&:nodes)
+  end
+
+  def other_paths(path)
+    current_paths[current_paths.index(path) + 1..-1] + sub_paths
   end
 end
